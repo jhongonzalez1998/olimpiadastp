531de91d4a1a3642f4acb818247c1767c8be44fa
jhongonzalez1998 <134099431+jhongonzalez1998@users.noreply.github.com>
2026-07-12 02:24:50 -0500

    Delete js/supabase-config.js

--- js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Configuración de la conexión con Supabase
-const SUPABASE_URL = "https://tccfaxjfwbcpqcfyxvql.supabase.co"; // Cambia esto
-const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjY2ZheGpmd2JjcHFjZnl4dnFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4MjIwNjYsImV4cCI6MjA5OTM5ODA2Nn0.vXnP5psPFPjWY4dGJMgNYsseQHtv7y2jUxytD-s0meI"; // Cambia esto
-
-// Inicializar el cliente global que usarán los otros scripts
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
